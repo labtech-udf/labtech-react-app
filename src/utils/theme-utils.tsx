58a21e0dead097a5b0b00d818d59196e76cb7f84
Taui Silva <tauisilva@gmail.com>
2023-10-10 23:44:59 -0300

    Update theme-utils.tsx

--- src/utils/theme-utils.tsx
+++ src/utils/theme-utils.tsx
@@ -1,7 +1,6 @@
 export const changeTheme = () => {
     const themesLink = document.getElementById('theme-link') as HTMLLinkElement;
     const themeSelected = localStorage.getItem('theme');
-    console.log(themeSelected, themesLink);
     if(themesLink){
         themesLink.href = '/src/assets/themes/'+ themeSelected +'.css'; 
     }
